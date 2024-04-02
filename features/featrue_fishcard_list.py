fa7071bcc81a84c37a3f3b58d903c2534340150d
KamciaRamcia <siostra264@gmail.com>
2024-04-02 14:04:13 +0200

    dodanie funkcjonalnosci usun liste fiszek

--- features/featrue_fishcard_list.py
+++ features/featrue_fishcard_list.py
@@ -12,27 +12,44 @@ class NewListElement:
     def __init__(self,fishcard_set_name,root):
         self.fishcard_set_name=fishcard_set_name
         self.root = root
+        self.lista_buttonow=[]
+
 
     def create_elements(self):
 
         text = importlib.import_module(f'tlumaczenia.{self.fishcard_set_name}') #sciezka w formie textu do przycisku PLAY
-        label_name=tk.Label(self.root, text=self.fishcard_set_name).grid(row=NewListElement.start_row, column=1)
-        button_play=tk.Button(self.root, text="Play Game", command=lambda: interface_memo_game.memoGame(text.slownik)).grid(row=NewListElement.start_row,column=6)
-        button_remove = tk.Button(self.root, text="Remove fishcard set", command=lambda: self.delete_fishcard()).grid(row=NewListElement.start_row, column=7)
+        label_name=tk.Label(self.root, text=self.fishcard_set_name)
+        label_name.grid(row=NewListElement.start_row, column=1)
+        button_play=tk.Button(self.root, text="Play Game", command=lambda: interface_memo_game.memoGame(text.slownik))\
+
+        self.lista_buttonow.append(button_play)
+        button_play.grid(row=NewListElement.start_row, column=6)
+        button_remove = tk.Button(self.root, text="Remove fishcard set", command=lambda: self.delete_fishcard(button_play, button_remove, label_name))
+        self.lista_buttonow.append(button_remove)
+        button_remove.grid(row=NewListElement.start_row, column=7)
         NewListElement.start_row = + 1
+        print(self.lista_buttonow)
 
-    def delete_fishcard(self):
-        print('****')
-        print(os.listdir())
 
-        sciezkaDoPliku = "../tlumaczenia/slowa.py"
+
+    def delete_fishcard(self,button_play, button_remove,label_name):
+
+        button_play.destroy()
+        button_remove.destroy()
+        label_name.destroy()
+
+        sciezkaDoPliku = f"../tlumaczenia/{self.fishcard_set_name}.py" # usuwa sie na serio
         if os.path.isfile(sciezkaDoPliku):
-            os.unlink(sciezkaDoPliku)
+             os.unlink(sciezkaDoPliku)
+             self.delete_button()
+
         else:
             print("Wybacz, plik nie istnieje :(")
 
+
     def delete_button(self):
         pass
+
 
 
 
--- interface/interface_fishcard_list.py
+++ interface/interface_fishcard_list.py
@@ -6,19 +6,32 @@ from features import featrue_fishcard_list  as ffl
 
 from interface.interface_memo_game import memoGame
 
+start_row=3
 def listOfFiles():
     files = os.listdir('../tlumaczenia/')
     files_file = [f for f in files if os.path.isfile(os.path.join('../tlumaczenia/', f))]
     return files_file
 
-def create_one_row(root,fishcard_name):
-    #Label(root, text="Path to your csv file: ").grid(row=3, column=1)
+def get_play_button():
     pass
+
+def get_delete_button():
+    pass
+
+def create_one_row(root,fishcard_name,start_row):
+        text = importlib.import_module(f'tlumaczenia.{fishcard_name}') #sciezka w formie textu do przycisku PLAY
+        label_name = Label(root, text=fishcard_name).grid(row=start_row, column=1)
+        button_play=Button(root, text="Play Game", command=lambda: interface_memo_game.memoGame(text.slownik)).grid(row=start_row,column=6)
+        #button_remove = Button(root, text="Remove fishcard set", command=lambda: delete_fishcard()).grid(row=start_row, column=7)
+        start_row = + 1
+
+
 
 
 def generateFishcardList(): # ZMIEN NAZW NA ADD_FISHCARD
     root = Tk()
     root.geometry("600x600")
+
 
 
     Label(root,text="Lista Twoich fiszek",bg='blue').grid(row=1, column=2)
@@ -27,6 +40,7 @@ def generateFishcardList(): # ZMIEN NAZW NA ADD_FISHCARD
         print(file)
         dictionary_name = file[:-3]
         print(dictionary_name)
+        #create_one_row(root,dictionary_name,start_row)
         ffl.NewListElement(dictionary_name,root).create_elements()
 
 

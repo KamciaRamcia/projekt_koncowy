0eabe9801fc415b7e3c09696eb035b8fb5f8c6ac
KamciaRamcia <siostra264@gmail.com>
2024-04-12 19:51:35 +0200

    Create interface_winner_window.py
    dodaj skrypt winner window

---
+++ interface/interface_winner_window.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import interface.interface_main as im
+import interface.interface_memo_game as img
+
+
+
+def winnerInformation(green_counter, red_counter):   # zmien nazwe na memo_game
+    root = Tk()
+    root.geometry("300x300")
+    root.config(bg="skyblue")
+    Label(root, text=f'Wygrałeś!').pack()
+    Label(root, text=f'Twoja liczka ruchów to: {red_counter + green_counter}').pack()
+    Button(root, text='Wróć do menu głównego', command= lambda: back_to_menu_actions()).pack()
+    def back_to_menu_actions():
+        root.destroy()
+        im.FirstPage0(Tk()).get_run_first_page()
+       # env.destroy()
+
+
+
+
+
+    root.mainloop()

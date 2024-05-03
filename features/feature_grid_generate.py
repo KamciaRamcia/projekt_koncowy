e9bc111896a409e0d008fc4e50e4a84044559097
KamciaRamcia <siostra264@gmail.com>
2024-05-03 21:42:26 +0200

    Create feature_grid_generate.py

---
+++ features/feature_grid_generate.py
@@ -0,0 +1,12 @@
+import math
+
+def calculateRowsQuantity(your_dictionary):
+    elements_quanity = len(your_dictionary)*2
+    print(elements_quanity)
+    row_quantity = math.ceil(math.sqrt(elements_quanity))
+    if elements_quanity>36:  # maksymalnie jest 6 kolumn
+        addtional_rows = math.ceil((elements_quanity -36)/6)
+        row_quantity = row_quantity+addtional_rows
+    return (row_quantity)
+
+
